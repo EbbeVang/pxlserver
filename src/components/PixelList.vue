16f17a208e31ec7f001ec62662c39d6ec6d420ea
Ebbe Vang <ev@evang.dk>
2020-03-13 11:35:58 +0100

    added dgram

--- src/components/PixelList.vue
+++ src/components/PixelList.vue
@@ -4,7 +4,7 @@
     list of Pixels
     <ul id="example-1">
       <li v-for="pixel in pixels" :key="pixel.name">
-        {{ pixel.name }}
+        {{ pixel }}
     </li>
     </ul>
   </div>
@@ -13,28 +13,37 @@
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { Pixel} from '../models/pixel';
+var dgram = require('dgram');
+
 
 @Component
 export default class PixelList extends Vue {
   @Prop() private msg!: string;
 
   public pixels: Pixel[] = [];
+  public socket: any;
 
   constructor()
   {
     super();
-    this.pixels.push(new Pixel(2,3,"ebbe","#ff0000"));
-    this.pixels.push(new Pixel(2,3,"bob","#0000ff"));
-    this.pixels.push(new Pixel(2,3,"john","#00ff00"));
-    this.pixels.push(new Pixel(2,3,"torben","#ffff00"));
+    this.pixels.push(new Pixel(2,3,"ebbe","#ff0000","1234"));
+    this.pixels.push(new Pixel(2,3,"bob","#0000ff","1234"));
+    this.pixels.push(new Pixel(2,3,"john","#00ff00","1234"));
+    this.pixels.push(new Pixel(2,3,"torben","#ffff00","1234"));
 
+    this.socket = dgram.createSocket('udp4');
+    this.socket.on('message', function(msg:any, rinfo:any) {
+      console.log('I got this message: ' + msg.toString());
+      //this.pixels.push(new Pixel(3,4,msg.toString(), "#ff0000","1234"))
+    });
+    this.socket.bind(7000);
 
-    window.setInterval(function(p: Pixel[]) {
+    /*window.setInterval(function(p: Pixel[]) {
       /// call your function here
       p.push(new Pixel(2,3,"torben","#ffff00"));
       console.log('hi');
     }, 500, this.pixels); 
-
+    */
   }
 }
 </script>
--- src/models/pixel.ts
+++ src/models/pixel.ts
@@ -1,15 +1,21 @@
 export class Pixel
 {
-    constructor(x: number, y: number, name: string, color: string)
+    constructor(x: number, y: number, name: string, color: string, ip: string)
     {
         this.x =x;
         this.y =y;
         this.name = name;
         this.color = color;
+        this.ip = ip
     }
 
     public x: number;
     public y: number;
     public name: string;
     public color: string;
+    public ip: string;
+
+    public toString = () : string => {
+        return this.name + " " + this.ip;
+    }
 }
